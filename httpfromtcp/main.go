17d105aa678bec059b4675e620d7bc179a035764
Quan Truong <qmtruong92@gmail.com>
2026-03-28 12:54:29 -0700

    C1L5: Channel Refactor

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -8,6 +8,43 @@ import (
 	"strings"
 )
 
+func getLinesFromChannel(file io.ReadCloser) <-chan string {
+
+	results := make(chan string)
+	buffer := make([]byte, 8)
+
+	currentLineContents := ""
+
+	go func() {
+		for {
+			n, err := file.Read(buffer)
+
+			if err != nil {
+				if currentLineContents != "" {
+					// fmt.Printf("read: %s\n", currentLineContents)
+					results <- currentLineContents
+					currentLineContents = ""
+				}
+				if errors.Is(err, io.EOF) {
+					close(results)
+					file.Close()
+					break
+				}
+				fmt.Printf("failed to read file: error: %v", err)
+				break
+			}
+			string := string(buffer[:n])
+			parts := strings.Split(string, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				results <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+				currentLineContents = ""
+			}
+			currentLineContents += parts[len(parts)-1]
+		}
+	}()
+	return results
+}
+
 func main() {
 	wd, err := os.Getwd()
 	if err != nil {
@@ -18,31 +55,9 @@ func main() {
 		fmt.Printf("error opening file: error: %v", err)
 	}
 	defer file.Close()
-
-	buffer := make([]byte, 8)
-
-	currentLineContents := ""
-	for {
-
-		n, err := file.Read(buffer)
-
-		if err != nil {
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
-				currentLineContents = ""
-			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("failed to read file: error: %v", err)
-			break
-		}
-		string := string(buffer[:n])
-		parts := strings.Split(string, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts)-1]
+	lines := getLinesFromChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
+
 }
